20cad9e03dd902751e9c07520603e975ce065e71
ANILKUMAR <anilkumarthalluri123@gmail.com>
2025-10-01 00:39:58 +0530

    FIX: Add JpaConfig to programmatically create DataSource

---
+++ src/main/java/com/chat/Real_Time/Chat/Application/config/JpaConfig.java
@@ -0,0 +1,34 @@
+package com.chat.Real_Time.Chat.Application.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        if (databaseUrl == null) {
+            throw new IllegalStateException("DATABASE_URL environment variable is not set.");
+        }
+
+        URI dbUri = new URI(databaseUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(username)
+                .password(password)
+                .driverClassName("org.postgresql.Driver")
+                .build();
+    }
+}
